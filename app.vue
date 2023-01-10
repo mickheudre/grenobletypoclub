a652c1e8a85295a01da44bda9bc69dfac9245301
mick <mickheudre@gmail.com>
2023-01-10 16:20:48 +0100

    ajout cabin, nuxt 3 style

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="max-w-xl mx-auto">
+    <h1 class="text-6xl font-brand text-green capitalize mt-12">GRENOBLE TYPO CLUB</h1>
+    <p class="text-green text-lg my-2">Coming Soon </p>
+  </div>
+</template>
+<script>
+export default {
+  setup () {
+    useHead({
+      script: [
+      { async: true,
+        src: 'https://scripts.withcabin.com/hello.js',
+      }]
+    }) },
+    head: {
+      title: 'Grenoble Typo Club'
+    }
+  }
+</script>
--- pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="max-w-xl mx-auto">
-      <h1 class="text-6xl font-brand text-green capitalize mt-12">GRENOBLE TYPO CLUB</h1>
-      <p class="text-green text-lg my-2">Coming Soon </p>
-    </div>
-  </template>
