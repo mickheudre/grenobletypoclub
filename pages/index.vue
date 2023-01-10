5024c6f7c186c2aeb27c8ba9d6f6a9c1df370859
mick <mickheudre@gmail.com>
2023-01-10 15:58:15 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="max-w-xl mx-auto">
+      <h1 class="text-6xl font-brand text-green capitalize mt-12">GRENOBLE TYPO CLUB</h1>
+      <p class="text-green text-lg my-2">Coming Soon </p>
+    </div>
+  </template>
