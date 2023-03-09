00e20f04ff06efe30074225858d4f8feb05e5f00
mick <mickheudre@gmail.com>
2023-03-09 12:54:21 +0100

    fix

--- components/TypoBig.vue
+++ components/TypoBig.vue
@@ -7,9 +7,9 @@
       </div>
       <div class="flex flex-row justify-center  w-full h-full">
         <div class="flex flex-col h-full gap-8 m-16 ">
-          <a :style="{ 'font-family': name }" :class="`text-${size}`" class="text-green">a b c d e f g h i j k l m n o p q r s t u v w x y z</a>
-          <a :style="{ 'font-family': name }" :class="`text-${size}`" class="text-green capitalize ">a b c d e f g h i j k l m n o p q r s t u v w x y z</a>
-          <a :style="{ 'font-family': name }" :class="`text-${size}`" class="text-green">0 1 2 3 4 5 6 7 8 9</a>
+          <a :style="{ 'font-family': name }" :class="styling" class="text-green">a b c d e f g h i j k l m n o p q r s t u v w x y z</a>
+          <a :style="{ 'font-family': name }" :class="styling" class="text-green capitalize ">a b c d e f g h i j k l m n o p q r s t u v w x y z</a>
+          <a :style="{ 'font-family': name }" :class="styling" class="text-green">0 1 2 3 4 5 6 7 8 9</a>
 
           <div class="flex justify-center">
           <a  :href="`/${name}.zip`" :download="`${name}.zip`"><Download class="h-20 w-20 fill-green"/></a>
@@ -17,7 +17,7 @@
         </div>
         <div class="relative w-12">
           <div class="absolute left-0 -translate-x-1/2 translate-y-1/2 inset-y-0 ">
-            <p :style="{ 'font-family': name }" :class="`text-${size}`" class=" w-max text-green rotate-90 origin-center">{{ text }}</p>
+            <p :style="{ 'font-family': name }" :class="styling" class=" w-max text-green rotate-90 origin-center">{{ text }}</p>
           </div>
         </div>
       </div>
@@ -25,11 +25,18 @@
 </template>
 
 <script setup lang="ts">
+
+
 export interface Typo {
     name: string,
     text: string,
     size: string
 }
+const styling = computed(() => ({
+  'text-2xl': props.size == "2xl",
+  'text-xl': props.size == "xl"
+}))
+
 const props = defineProps<Typo>()
 
 </script>
