53b632e09ab643ec89deaea866a9ff9675f2c680
mick <mickheudre@gmail.com>
2023-05-24 12:51:19 +0200

    Ajout de description alt sur les images

--- components/Notion/Image.vue
+++ components/Notion/Image.vue
@@ -1,11 +1,10 @@
 <template>
-    <img class="notion-img" :src="compurl" />
+    <img class="notion-img" :src="compurl" :alt="props.link.image.caption.length > 0 ? props.link.image.caption[0].plain_text : 'Une image sans description'"/>
 </template>
 
 <script setup lang="ts">
 
 const props = defineProps(['link'])
-
 const compurl = computed(() => {
     
     if (process.env.NODE_ENV == "production" || process.env.NODE_ENV == "prerender") {
